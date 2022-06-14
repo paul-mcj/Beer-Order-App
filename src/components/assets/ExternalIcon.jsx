7830fcd1b091ec76a788735ed8a11a7cb280d9c6
paul-mcj <83841390+paul-mcj@users.noreply.github.com>
2022-06-14 17:04:41 -0400

    finalized sorting function reducer logic, and updated comments on various components

---
+++ src/components/assets/ExternalIcon.jsx
@@ -0,0 +1,17 @@
+// // note: component is unused!
+
+// //components
+// import IconWrapper from "../layout/IconWrapper";
+
+// // react-icons
+// import { BiLinkExternal as MoreInfo } from "react-icons/bi";
+
+// const ExternalIcon = () => {
+//     return (
+//         <IconWrapper>
+//             <MoreInfo className="text-accent hover:text-accent-focus" />
+//         </IconWrapper>
+//     );
+// };
+
+// export default ExternalIcon;

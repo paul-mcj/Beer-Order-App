34b42403d7dc7ec85e11624c99f4fc2c134ef360
unknown <mcjannetp@gmail.com>
2025-12-20 13:55:32 -0500

    update: tailwind class for margin of 2xl screen pinched layout and loked bad, reverted changes

--- src/components/layout/PageWrapper.jsx
+++ src/components/layout/PageWrapper.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const PageWrapper = ({ children }) => {
 	return (
-		<section className="px-10 py-14 tablet:px-16 lg:px-52 xl:px-72 2xl:px-96 min-h-screen bg-base-100">
+		<section className="px-10 py-14 tablet:px-16 lg:px-52 xl:px-72 min-h-screen bg-base-100">
 			{children}
 		</section>
 	);
